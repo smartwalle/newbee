e3bf3a055c6142603b17dbb009f61557956c393a
smartwalle <smartwalle@gmail.com>
2020-02-14 15:54:48 +0800

    优化代码

--- room.go
+++ room.go
@@ -395,6 +395,7 @@ func (this *room) RemovePlayer(playerId uint64) {
 	var player = this.GetPlayer(playerId)
 	if player != nil {
 		this.mu.Lock()
+		player.Conn().UpdateHandler(nil)
 		delete(this.players, player.GetId())
 		this.mu.Unlock()
 	}
